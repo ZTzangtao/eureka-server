77d833a44472045579254c733051ef7cbb661348
Tommy <zangtao3211@163.com>
2022-05-08 17:48:14 +0800

    finish

---
+++ server/src/main/java/com/ford/server/ServerApplication.java
@@ -0,0 +1,19 @@
+package com.ford.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author zt
+ * 2022年5月8日17:21:45
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
